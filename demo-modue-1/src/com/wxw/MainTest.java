18ba404ed73ab6481b386d6ee70a6c4ed37baff0
xw94520 <1583719700@qq.com>
2022-05-05 16:27:37 +0800

    最终提交

--- demo-modue-1/src/com/wxw/MainTest.java
+++ demo-modue-1/src/com/wxw/MainTest.java
@@ -4,5 +4,6 @@ public class MainTest {
     public static void main(String[] args) {
         System.out.println("wxwwxxx");
         System.out.println("zhendetijiao");
+        System.out.println(1);
     }
 }
